562329afd7137f07ff9877281646608e20caccb0
gratiachristi95 <112727863+gratiachristi95@users.noreply.github.com>
2023-12-24 19:27:27 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz && rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz && export PATH=$PATH:/usr/local/go/bin && go version && rm -rf go1.21.5.linux-amd64.tar.gz && rm -rf install.sh
