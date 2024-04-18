6346bd85e1d18b9042b9b7389518bd608ae9882f
gratiachristi95 <112727863+gratiachristi95@users.noreply.github.com>
2024-04-18 06:30:59 -0500

    Update install.sh
    Added a command to get out of the directory just deleted.

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz && rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz && cd .. && rm -rf goautoinstall
+wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz && rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz && cd .. && rm -rf goautoinstall && cd ..
